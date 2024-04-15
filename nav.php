f23fa299838c6ed273cf472e6830809adf1796d1
Emma <ebrenna2@mail.umw.edu>
2024-04-15 19:21:03 -0400

    added new files

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li><a href="Marvel_Pookies.php">Home</a></li>
+        <li><a href="movies.php">Movies</a></li>
+        <li><a href="tv.php">TV Shows</a></li>
+        <li><a href="superheroes.php">Superheroes</a></li>
+        <li><a href="villains.php">Villains</a></li>
+        <li><a href="tvmovies.php">TV + Movie Appearances</a></li>
+    </ul>
+</nav>
+
