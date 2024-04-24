4098684c8d507cb03afeaa712a42f8e47e53d0ad
Emma <ebrenna2@mail.umw.edu>
2024-04-24 10:05:18 -0400

    edited

--- register.php
+++ register.php
@@ -2,7 +2,6 @@
 session_start();
 require_once "credentials.php";
 
-// Handling the registration post data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
